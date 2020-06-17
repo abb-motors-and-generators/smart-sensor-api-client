127c1f553f251aa44157fc49aac1daa8a1597cf8
Dominik Frey <dominik.frey@ch.abb.com>
2020-06-17 18:27:24 +0200

    Added post request function and implemented "add_notification_channel"

---
+++ use_cases/use_case_6_add_notification_channel.md
@@ -0,0 +1,39 @@
+# Use Case 6: Add Notification Channel
+
+This code demonstrates how to add a notification channel to a specific asset
+
+If you have not configured your credentials in the `settings.yaml` file in the root folder,
+the program will ask you to input your username, password, or API key.
+
+To run the code, execute:
+
+    python use_case_6_add_notification_channel.py >ASSET ID LIST< -nt >NOTIFICATION TYPE< -nc >NOTIFICATION CHANNEL< -u >URL<
+    
+For example:
+
+    python use_case_6_add_notification_channel.py 1234 4567 -nt 13 -nc 3 -u https://webhook.site/bcf22b9e-9a30-45c2-890b-a9b948XXXXXX
+    
+## Arguments
+### Asset ID List
+Defines to which assets this notification channel should apply.
+
+### Notification Type
+Defines, what kind of notifications should be set up for the defined assets.
+- load measurement alert = 2
+- load measurement alarm = 3,
+- measurement taken reminder = 5        
+- asset nameplate data edited = 9        
+- low battery capacity = 12        
+- load measurement notification = 13        
+
+### Notification Channel
+Defines the interface where the notification should be sent
+- webhooks = 3
+
+### URL
+The URL is the endpoint of the notification. You can use your own server, but for testing purposes we suggest to use the website [https://webhook.site](https://webhook.site).
+1. Go to [https://webhook.site](https://webhook.site)
+2. Copy `Your unique URL`
+3. Use this URL as an argument
+4. See the notifications in the left column of the website
+![Screenshot Webhook.site](../webhooksite.png "Screenshot of Webhook.site")
---
+++ use_cases/use_case_6_add_notification_channel.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+"""Example 6
+
+This code demonstrates how to sign up a webhook which connects an external URL with the smartsensor platform
+
+Example:
+    $ python use_case_6_add_notification_channel.py *ASSETID*
+
+"""
+
+from smart_sensor_client.smart_sensor_client import SmartSensorClient
+import argparse
+
+DEFAULT_SETTINGS_FILE = 'settings.yaml'
+
+
+def run_task(settings_file=DEFAULT_SETTINGS_FILE, asset_list=None, notification_type=None, notification_channel=None, url=None) -> bool:
+    # Create the client instance
+    client = SmartSensorClient(settings_file=settings_file)
+
+    # Authenticate
+    if not client.authenticate():
+        print('Authentication FAILED')
+        return False
+
+    response = client.add_notification_channel(notification_type, notification_channel, asset_list, url)
+
+    if response is False:
+        return False
+
+    return True
+
+
+# Main body
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Add a notification channel to the smart sensor platform.')
+    parser.add_argument('asset_list', metavar='asset', type=int, nargs='+', help='possibly multiple asset IDs for which the notification should be setup')
+    parser.add_argument('-nt', '--notification_type', type=int, help='an integer for the notification type')
+    parser.add_argument('-nc', '--notification_channel', type=int, help='an integer for the notification channel')
+    parser.add_argument('-u', '--url', type=str, help='an url where the webhook should be attached to')
+    args = parser.parse_args()
+
+    result = run_task(asset_list=args.asset_list, notification_type=args.notification_type, notification_channel=args.notification_channel, url=args.url)
+
+    if result is True:
+        print('Task SUCCESS')
+    else:
+        print('Task FAILED')
+
